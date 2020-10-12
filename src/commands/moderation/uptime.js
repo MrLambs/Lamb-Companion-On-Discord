159053d0f4d5126138ea921015c2c90497b7375c
Matt Lambert <50882540+MrLambs@users.noreply.github.com>
2020-10-11 23:11:22 -0700

    ⌚ Added uptime.js
    > Created uptime command to inform me and other members on how long the bot has been running.

---
+++ src/commands/moderation/uptime.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    config: {
+        name: "uptime",
+        description: "Displays the bots current uptime!",
+        usage: ``,
+        accessableby: "Members",
+        aliases: ["ut"],
+        category: "moderation"
+    },
+    run: async (bot, message, args) => {
+        try {
+            const duration = ms => {
+                const sec = Math.floor((ms / 1000) % 60).toString();
+                const min = Math.floor((ms / (1000 * 60)) % 60).toString();
+                const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString();
+                return `\`\`${hrs.padStart(2, '0')}\`\` **hours**, \`\`${min.padStart(2, '0')}\`\` **minutes**, \`\`${sec.padStart(2, '0')}\`\` **seconds**.`
+            };
+
+            return message.channel.send(new MessageEmbed().setColor("GREEN").setDescription(`I have been online for: ${duration(bot.uptime)}`));
+        } catch (e) {
+            console.log(`[ERR] ${e.message}`)
+        }
+    }
+};
