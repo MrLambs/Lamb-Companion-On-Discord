2ceb290e441e863ad539a76e8921102795f87a42
Matt Lambert <50882540+MrLambs@users.noreply.github.com>
2020-10-28 16:13:02 -0700

    Removed bug from messageReaction event listeners

--- src/events/guild/messageReactionRemove.js
+++ src/events/guild/messageReactionRemove.js
@@ -6,8 +6,7 @@ module.exports = async (bot, reaction, user) => {
     let message = reaction.message,
         emoji = reaction.emoji.name,
         member = message.guild.members.fetch(user.id)
-    if (!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(new MessageEmbed().setColor("RED").setDescription(":x: I do not have permission to add roles.")).then(m => m.delete({ timeout: 5000 }))
-    else if (!message.guild.id === '688514030117453895') return;
+    if (!message.guild.id === '688514030117453895') return;
     else {
         fetchOsrsRoles(message).then(roles => {
             determineRoleToRemove(member, message, emoji, roles)
