e8600b9bc8d6109a03cbcfe6d0d544365fbce1e6
Matt Lambert <50882540+MrLambs@users.noreply.github.com>
2020-12-16 18:22:16 -0800

    Work.js hotfix
    Updated daily work bonus timestamp trigger to more effectively reset workCounter

--- src/commands/casino/work.js
+++ src/commands/casino/work.js
@@ -19,7 +19,7 @@ module.exports = {
 
         User.findOne({ user_id: message.author.id })
             .then(user => {
-                if (user.worked == null) {
+                if (user.worked == null || user.worked == undefined) {
                     user.worked = {
                         tenMinute: null,
                         dailyResetTimeout: null,
@@ -27,50 +27,39 @@ module.exports = {
                     }
                 }
                 let worked = user.worked
-
                 let localWorkedCounter,
                     dailyDate = Date.now();
+
                 if (!worked.workedCounter) localWorkedCounter = 0
                 else localWorkedCounter = worked.workedCounter
-
-                console.log(dailyReset - (Date.now() - worked.dailyResetTimeout))
-                let dailyTimeLeft = ms(worked.dailyResetTimeout - (Date.now() - worked.dailyResetTimeout))
-                console.log(`${dailyTimeLeft.hours}h${dailyTimeLeft.minutes}m${dailyTimeLeft.seconds}s`)
+                let dailyTimeLeft = ms(dailyReset - (Date.now() - worked.dailyResetTimeout))
 
                 if ((worked.dailyResetTimeout !== null || worked.dailyResetTimeout !== undefined)
-                    && (worked.tenMinuteResetTimeout !== null || worked.tenMinuteResetTimeout !== undefined)
+                    && (worked.tenMinuteResetTimeout !== null || worked.tenMinuteTimeout !== undefined)
                     && dailyReset - (Date.now() - worked.dailyResetTimeout) > 0) {
                     user.worked = {
                         ...user.worked,
                         workedCounter: localWorkedCounter + 1
                     }
                 } else {
+                    localWorkedCounter = 0
                     user.worked = {
-                        tenMinuteResetTimeout: null,
+                        tenMinuteResetTimeout: dailyDate,
                         dailyResetTimeout: dailyDate,
-                        workedCounter: 0
+                        workedCounter: localWorkedCounter
                     }
                 }
-
 
                 if (worked.tenMinuteResetTimeout !== null && tenMinute - (Date.now() - worked.tenMinuteResetTimeout) > 0) {
                     let time = ms(tenMinute - (Date.now() - worked.tenMinuteResetTimeout));
                     return message.channel.send(new MessageEmbed().setColor("RED").setDescription(`:x: You cannot work again for: **${time.minutes}m** and **${time.seconds}s**`))
                 } else {
-                    let currentCounter;
-                    if (Number(worked.workedCounter)) currentCounter = worked.workedCounter
-                    else currentCounter = 0;
 
-                    counterIncrease = currentCounter + 1.
-                    percentageIncrease = (currentCounter * 2.5) / 100;
+                    counterIncrease = localWorkedCounter + 1.
+                    percentageIncrease = (localWorkedCounter * 2.5) / 100;
 
                     lambiesAmount += Math.ceil(lambiesAmount * percentageIncrease);
                     xpAmount += Math.ceil(xpAmount * percentageIncrease);
-                    user.worked = {
-                        ...user.worked,
-                        tenMinuteResetTimeout: Date.now(),
-                        workedCounter: counterIncrease
-                    }
                     user.money += lambiesAmount;
                     user.xp += xpAmount
                     user.save()
@@ -82,7 +71,7 @@ module.exports = {
                     if (!dailyTimeLeft || dailyTimeLeft.hours < 0 || dailyTimeLeft.minutes < 0 || dailyTimeLeft.seconds < 0) workEmbed.setFooter(`Cannot work again for 10 minutes`)
                     else workEmbed.setFooter(`Cannot work again for 10 minutes - Bonus will reset in ${dailyTimeLeft.hours}h ${dailyTimeLeft.minutes}m ${dailyTimeLeft.seconds}s`)
 
-                    if (currentCounter > 0) workEmbed.setAuthor(`${message.author.username} just worked their a** off and earned a ${(percentageIncrease * 100).toFixed()}% bonus!`, message.author.displayAvatarURL({ format: 'png', dynamic: true }))
+                    if (localWorkedCounter > 0) workEmbed.setAuthor(`${message.author.username} just worked their a** off and earned a ${(percentageIncrease * 100).toFixed()}% bonus!`, message.author.displayAvatarURL({ format: 'png', dynamic: true }))
                     else workEmbed.setAuthor(`${message.author.username} just worked their a** off!`, message.author.displayAvatarURL({ format: 'png', dynamic: true }))
 
                     return message.channel.send(workEmbed)
